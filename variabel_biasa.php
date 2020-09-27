500d33dc2e80905bfabe7c60c622febfc97230f2
ainaillyyin <71957400+ainaillyyin@users.noreply.github.com>
2020-09-27 14:57:07 +0700

    Create variabel_biasa.php

---
+++ variabel_biasa.php
@@ -0,0 +1,15 @@
+<?php
+$nama = "Aina Illyyin";
+$nim = 2018804475;
+$kelas = "SI malam 2018";
+$Kampus = "STMIK Insan Pembangunan F";
+echo "Nama Mahasiswa : $nama";
+echo "<br>";
+echo "Nim Mahasiswa : $nim";
+echo "<br>";
+echo "Kelas : $kelas";
+echo "<br>";
+echo "Nama Kampus : $Kampus";
+echo "<br>";
+
+?>
